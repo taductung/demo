af1ddf50fcfe859e73f798404f02b53b8994cdb0
byonls <byonls.hcm@gmail.com>
2015-05-18 12:16:26 +0700

    commit jave and jsp only

--- QAProject/src/main/java/com/qaproject/controller/HelloController.java
+++ QAProject/src/main/java/com/qaproject/controller/HelloController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class HelloController {
 	@RequestMapping(value = "/",method = RequestMethod.GET)
      public String printWelcome(ModelMap model) {
+        //test commit
         return "homepage";
     }
     @RequestMapping(value = "/home",method = RequestMethod.GET)
